b72367866a9d1987a884b70f6d6d421a96b745f0
Murilo <eu.murilok@gmail.com>
2024-11-02 19:58:20 -0300

    Adicionado routes user

---
+++ src/routes/user.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { UserController } from "../controllers/user";
+
+const controller = new UserController();
+const router = express.Router();
+
+router.post("/user", controller.create);
+router.get("/users", controller.getAll);
+router.get("/user/:id", controller.getById);
+router.put("/user/:id", controller.update);
+router.delete("/user/:id", controller.delete);
+
+export default router;
