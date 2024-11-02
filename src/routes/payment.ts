3d73ef783f68269d40ffcd687cd683e9c542023f
Murilo <eu.murilok@gmail.com>
2024-11-02 20:37:05 -0300

    Modificado rotas

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -4,10 +4,10 @@ import { PaymentController } from "../controllers/payment";
 const controller = new PaymentController();
 const router = express.Router();
 
-router.post("payment", controller.create);
-router.get("payments", controller.getAll);
-router.get("payment/:id", controller.getById);
-router.put("payment/:id", controller.update);
-router.delete("payment/:id", controller.delete);
+router.post("/payment", controller.create);
+router.get("/payments", controller.getAll);
+router.get("/payment/:id", controller.getById);
+router.put("/payment/:id", controller.update);
+router.delete("/payment/:id", controller.delete);
 
 export default router;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,10 +4,10 @@ import { UserController } from "../controllers/user";
 const controller = new UserController();
 const router = express.Router();
 
-router.post("user", controller.create);
-router.get("users", controller.getAll);
-router.get("user/:id", controller.getById);
-router.put("user/:id", controller.update);
-router.delete("user/:id", controller.delete);
+router.post("/user", controller.create);
+router.get("/users", controller.getAll);
+router.get("/user/:id", controller.getById);
+router.put("/user/:id", controller.update);
+router.delete("/user/:id", controller.delete);
 
 export default router;
