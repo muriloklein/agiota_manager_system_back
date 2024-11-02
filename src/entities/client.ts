6f89ca0f02a0309cf43c279b07e6d9ab4ddd936d
Murilo <eu.murilok@gmail.com>
2024-11-02 17:57:30 -0300

    Adicionado entitie de client

---
+++ src/entities/client.ts
@@ -0,0 +1,58 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Bill } from "./Bill";
+import { User } from "./user";
+
+@Entity()
+export class Client {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column()
+  name?: string;
+
+  @Column()
+  phone?: string;
+
+  @Column({ nullable: true })
+  address?: string;
+
+  @ManyToOne(() => User, (loanShark) => loanShark.clients)
+  loanShark?: User;
+
+  @OneToMany(() => Bill, (bill) => bill.client)
+  bills?: Bill[];
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+
+  constructor(
+    id?: number,
+    name?: string,
+    phone?: string,
+    address?: string,
+    loanShark?: User,
+    bills?: Bill[],
+    createdAt?: Date,
+    updatedAt?: Date
+  ) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.address = address;
+    this.loanShark = loanShark;
+    this.bills = bills;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
