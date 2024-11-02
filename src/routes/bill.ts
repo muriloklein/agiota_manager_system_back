5f28d344007fd1940cc59f5e9349f612d52ba90c
Murilo <eu.murilok@gmail.com>
2024-11-02 20:24:18 -0300

    Corrigido rotas

--- src/routes/bill.ts
+++ src/routes/bill.ts
@@ -4,10 +4,10 @@ import { BillController } from "../controllers/bill";
 const controller = new BillController();
 const router = express.Router();
 
-router.post("/bill", controller.create);
-router.get("/bills", controller.getAll);
-router.get("/bill/:id", controller.getById);
-router.put("/bill/:id", controller.update);
-router.delete("/bill/:id", controller.delete);
+router.post("bill", controller.create);
+router.get("bills", controller.getAll);
+router.get("bill/:id", controller.getById);
+router.put("bill/:id", controller.update);
+router.delete("bill/:id", controller.delete);
 
 export default router;
--- src/routes/client.ts
+++ src/routes/client.ts
@@ -4,10 +4,10 @@ import { ClientController } from "../controllers/client";
 const controller = new ClientController();
 const router = express.Router();
 
-router.post("/client", controller.create);
-router.get("/clients", controller.getAll);
-router.get("/client/:id", controller.getById);
-router.put("/client/:id", controller.update);
-router.delete("/client/:id", controller.delete);
+router.post("client", controller.create);
+router.get("clients", controller.getAll);
+router.get("client/:id", controller.getById);
+router.put("client/:id", controller.update);
+router.delete("client/:id", controller.delete);
 
 export default router;
