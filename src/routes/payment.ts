a6fa5eed90cd6416ae2db1de294c2094ed928071
Murilo <eu.murilok@gmail.com>
2024-11-02 19:58:11 -0300

    Adicionado routes payment

---
+++ src/routes/payment.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { PaymentController } from "../controllers/payment";
+
+const controller = new PaymentController();
+const router = express.Router();
+
+router.post("/payment", controller.create);
+router.get("/payments", controller.getAll);
+router.get("/payment/:id", controller.getById);
+router.put("/payment/:id", controller.update);
+router.delete("/payment/:id", controller.delete);
+
+export default router;
