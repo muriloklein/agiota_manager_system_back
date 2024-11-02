d20fbcb359550b717506434a591551da90f89da9
Murilo <eu.murilok@gmail.com>
2024-11-02 17:56:59 -0300

    Adicionado middleware errorHandler

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  console.log(err);
+  res.status(500).json({ error: "Internal Server Error" });
+};
+
+export default errorHandler;
