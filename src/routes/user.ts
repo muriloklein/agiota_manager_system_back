8406dbb7bc7eb475f1b04105b4ff303720017404
Murilo <eu.murilok@gmail.com>
2024-11-06 21:59:27 -0300

    Corrigido as rotas de user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,10 +4,7 @@ import { UserController } from "../controllers/user";
 const controller = new UserController();
 const router = express.Router();
 
-router.post("/user", controller.create);
-router.get("/users", controller.getAll);
-router.get("/user/:id", controller.getById);
-router.put("/user/:id", controller.update);
-router.delete("/user/:id", controller.delete);
+router.post("/login", controller.login);
+router.post("/register", controller.create);
 
 export default router;
